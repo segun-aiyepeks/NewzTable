3d9bae2a65f5b4a0a7a98c82cdf8a9830fff3369
segun-aiyepeks <segunaiyepeks@gmail.com>
2026-08-10 12:14:27 +0100

    Added TopicModel

---
+++ web/Models/TopicModel.cs
@@ -0,0 +1,8 @@
+﻿namespace web.Models
+{
+    public class TopicModel
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+    }
+}
