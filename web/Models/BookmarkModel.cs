4ae751d37af94b161cb1b5eae3f079e7554a263e
segun-aiyepeks <segunaiyepeks@gmail.com>
2026-08-11 10:26:19 +0100

    Added BookmarkModel to web

---
+++ web/Models/BookmarkModel.cs
@@ -0,0 +1,9 @@
+﻿namespace web.Models
+{
+    public class BookmarkModel
+    {
+        public string BookmarkdId { get; set; } = string.Empty;
+        public DateTime SavedAt { get; set; }
+        public ArticleModel Article { get; set; }
+    }
+}
