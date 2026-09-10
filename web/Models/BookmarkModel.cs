cee882d80b6568a319f90aeba0d1424a54a8a679
segun-aiyepeks <segunaiyepeks@gmail.com>
2026-09-10 12:22:34 +0100

    removed Blazor web app, switching to Bootstrap + Vanilla JS

--- web/Models/BookmarkModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace web.Models
-{
-    public class BookmarkModel
-    {
-        public string BookmarkdId { get; set; } = string.Empty;
-        public DateTime SavedAt { get; set; }
-        public ArticleModel Article { get; set; }
-    }
-}
--- web/Models/TopicModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace web.Models
-{
-    public class TopicModel
-    {
-        public string Key { get; set; } = string.Empty;
-        public string Label { get; set; } = string.Empty;
-    }
-}
